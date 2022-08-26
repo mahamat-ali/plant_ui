92bd35bcb63a21c3e2146b1083b0295623f63774
Mahamat Ali <terapfils30@gmail.com>
2022-08-26 22:00:36 +0100

    build header with search box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:plant_app_zero/constants.dart';
+
+import 'components/header_with_searchbox.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: buildAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderWithSearchbox(size: size),
+          ],
+        ),
+      ),
     );
   }
 
